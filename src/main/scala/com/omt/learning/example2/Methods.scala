76cb706be64ff71cd0f3334df135f39b2a3687b3
dhpandya <dhpandya@ebay.com>
2019-01-13 16:08:49 -0800

    Initial

--- src/main/scala/com/omt/learning/example2/Methods.scala
+++ src/main/scala/com/omt/learning/example2/Methods.scala
@@ -10,6 +10,14 @@ object Methods {
     println(obj.max(2,7))
     println(obj.noParam)
     obj.equalSign
+    
+    println(obj.returnSameTHING(2,7))
+    println(obj.returnAnyValDym(2,7))
+    
+    var a:Int = obj.returnAnyValDym(27,7).asInstanceOf[Int]
+    
+    println(obj.defaultValueInParam("Dhiral"))
+    println(obj.defaultValueInParam("omt"))
   }
   
 }
@@ -28,6 +36,34 @@ class MethodsClass {
   def noParam:String = "No params no "
   
   def equalSign() = println("nothing")
+  
+  
+  def returnSameTHING(a:Int, b:Int) ={
+    if(a>b)
+      a
+    else 
+      b
+    //Last statement is the return one. 
+    "SAME STRING"
+  }
+  
+  def returnAnyValDym(a:Int,b:Int) = {
+     if(a>b) 
+       a
+     
+  }
+  
+  def changeParam(a:Int) ={
+    //a = a+1; error you can not re-assign
+  }
+  
+  
+  def defaultValueInParam(name:String = "omt"):Boolean = {
+     if(name.equals("omt"))
+        true
+      else
+       false
+  }
 }
 
 object MethodsClass{
